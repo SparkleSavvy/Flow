9fe83a3efa011567e2f427dd1c537b11b819ab6f
SparkleSavvy <140531552+SparkleSavvy@users.noreply.github.com>
2026-05-04 20:00:00 +0300

    Create preload.js

---
+++ preload.js
@@ -0,0 +1,15 @@
+const { contextBridge, ipcRenderer } = require('electron');
+
+contextBridge.exposeInMainWorld('api', {
+    checkDependencies: () => ipcRenderer.invoke('api-check'),
+    fetchMetadata: (url) => ipcRenderer.invoke('fetch-metadata', url),
+    downloadVideo: (data) => ipcRenderer.invoke('download-video', data),
+    onProgress: (callback) => ipcRenderer.on('download-progress', (event, value) => callback(value)),
+    
+    // Новые функции
+    selectFolder: () => ipcRenderer.invoke('select-folder'),
+    selectCookies: () => ipcRenderer.invoke('select-cookies'),
+    clearCookies: () => ipcRenderer.invoke('clear-cookies'),
+    getSettings: () => ipcRenderer.invoke('get-settings'),
+    getHistory: () => ipcRenderer.invoke('get-history')
+});
